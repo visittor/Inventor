480ba42834414e08dd03144eb7fe23c426ffc713
visittor <chattarin.rodpon@gmail.com>
2017-04-21 18:42:10 +0700

    add graphic and rfid thread

--- G6reader.py
+++ G6reader.py
@@ -7,27 +7,28 @@ import signal
 import time
 
 class Read_RFID:
+	def __init__(self):
+		self.MIFAREReader = MFRC522.MFRC522()
 
 	def get_uid(self,to=0):
 		print 'Waiting for RFIDtag'
 		# return uid in RFID
 		rfid_uid = []
 		# rfid_uid = [128,15,177,88]
-		MIFAREReader = MFRC522.MFRC522()
 		hex_uid = ''
 		t = time.clock()
 		if to != 0:
 			print 'Set timeout to',to,'sec' 
 
 		while True:
-			(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+			(status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
 			# If a card is found
-			if status == MIFAREReader.MI_OK:
+			if status == self.MIFAREReader.MI_OK:
 				print "RFIDtag detected"
             # Get the UID of the card
-			(status,uid) = MIFAREReader.MFRC522_Anticoll()
+			(status,uid) = self.MIFAREReader.MFRC522_Anticoll()
             # If we have the UID, continue
-			if status == MIFAREReader.MI_OK:
+			if status == self.MIFAREReader.MI_OK:
 				rfid_uid = uid
 				break
 			if time.clock()-t >= to:
@@ -46,5 +47,21 @@ class Read_RFID:
 		return rfid_uid
 
 		#return hex_uid
+	def read_once(self):
+		rfid_uid = []
+		hex_uid = ''
+		(status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
+		# If a card is found
+		if status == self.MIFAREReader.MI_OK:
+			print "RFIDtag detected"
+        # Get the UID of the card
+		(status,uid) = self.MIFAREReader.MFRC522_Anticoll()
+        # If we have the UID, continue
+		if status == self.MIFAREReader.MI_OK:
+			rfid_uid = uid
+		# for i in range(len(rfid_uid)):
+		# 	rfid_uid[i] = format(rfid_uid[i],'02x')
+		return rfid_uid
+
 	def end_read(self):
 		GPIO.cleanup()
