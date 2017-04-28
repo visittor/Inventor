4fa792c4f1b582aeebb0e78be883e42c6b049fde
visittor <chattarin.rodpon@gmail.com>
2017-04-28 14:28:10 +0700

    register rfid

---
+++ register_Rfid/register.py
@@ -0,0 +1,73 @@
+try:
+	from ConfigParser import ConfigParser
+except ImportError:
+	from configParser import ConfigParser #ver. > 3
+from G6reader import *
+
+parser = ConfigParser.SafeConfigParser()
+RR = Read_RFID()
+
+while True:
+	k = raw_input("r to add section, e to exit: ")
+	if k =='r':
+		k = raw_input("[Section number] = ")
+		sec_name = 'Food'+str(k)
+		try:
+			parser.add_section(sec_name)
+			uid = RR.get_uid()
+			parser.set(sec_name,'code',str(uid))
+			k = raw_input("Food name = ")
+			parser.set(sec_name,'name',k)
+			k = raw_input("Food protein = ")
+			parser.set(sec_name,'protein',k)
+			k = raw_input("Food carb = ")
+			parser.set(sec_name,'carb',k)
+			k = raw_input("Food fat = ")
+			parser.set(sec_name,'fat',k)
+			k = raw_input("Food energy = ")
+			parser.set(sec_name,'energy',k)
+			k = raw_input("Food vitA = ")
+			parser.set(sec_name,'vitA',k)
+			k = raw_input("Food vitD = ")
+			parser.set(sec_name,'vitD',k)
+			k = raw_input("Food vitE = ")
+			parser.set(sec_name,'vitE',k)
+			k = raw_input("Food vitK = ")
+			parser.set(sec_name,'vitK',k)
+			k = raw_input("Food vitC = ")
+			parser.set(sec_name,'vitC',k)
+			k = raw_input("Food vitB = ")
+			parser.set(sec_name,'vitB',k)
+		except DuplicateSectionError:
+			uid = RR.get_uid()
+			parser.set(sec_name,'code',str(uid))
+			k = raw_input("Food name = ")
+			parser.set(sec_name,'name',k)
+			k = raw_input("Food protein = ")
+			parser.set(sec_name,'protein',k)
+			k = raw_input("Food carb = ")
+			parser.set(sec_name,'carb',k)
+			k = raw_input("Food fat = ")
+			parser.set(sec_name,'fat',k)
+			k = raw_input("Food energy = ")
+			parser.set(sec_name,'energy',k)
+			k = raw_input("Food vitA = ")
+			parser.set(sec_name,'vitA',k)
+			k = raw_input("Food vitD = ")
+			parser.set(sec_name,'vitD',k)
+			k = raw_input("Food vitE = ")
+			parser.set(sec_name,'vitE',k)
+			k = raw_input("Food vitK = ")
+			parser.set(sec_name,'vitK',k)
+			k = raw_input("Food vitC = ")
+			parser.set(sec_name,'vitC',k)
+			k = raw_input("Food vitB = ")
+			parser.set(sec_name,'vitB',k)
+	elif k == 'e':
+		break
+
+with open("config.ini","a") as f:
+	parser.write(f)
+
+print "[OK]"
+
