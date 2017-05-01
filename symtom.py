1dae7aa8844a973cea075f4bb2736c4ab278adfd
visittor <chattarin.rodpon@gmail.com>
2017-05-01 10:39:30 +0700

    add music for every track

--- symtom.py
+++ symtom.py
@@ -2,17 +2,26 @@ from graphic import Sound,Non_thr_sond
 from input import Set_interrupt
 def symtomA(inhibitor):
 	if inhibitor['vitA'] < 0.5:
+		Set_interrupt.eat_Lock = 0
+		Non_thr_sond("Sound/Vit_A.mp3")
 		print "I'm lack of vitamin A"
+		Set_interrupt.eat_Lock = 1
 	return "vitA"
 
 def symtomD(inhibitor):
 	if inhibitor['vitD'] < 0.5:
+		Set_interrupt.eat_Lock = 0
+		Non_thr_sond("Sound/Vit_D.mp3")
 		print "I'm lack of vitamin D"
+		Set_interrupt.eat_Lock = 1
 	return "vitD"
 
 def symtomE(inhibitor):
 	if inhibitor['vitE'] < 0.5:
+		Set_interrupt.eat_Lock = 0
+		Non_thr_sond("Sound/Vit_E.mp3")
 		print "I'm lack of vitamin E"
+		Set_interrupt.eat_Lock = 1
 	return "vitE"
 
 def symtomK(inhibitor):
@@ -27,10 +36,16 @@ def symtomK(inhibitor):
 
 def symtomC(inhibitor):
 	if inhibitor['vitC'] < 0.5:
+		Set_interrupt.eat_Lock = 0
+		Non_thr_sond("Sound/Vit_C.mp3")
 		print "I'm lack of vitamin C"
+		Set_interrupt.eat_Lock = 1
 	return "vitC"
 
 def symtomB(inhibitor):
 	if inhibitor['vitB'] < 0.5:
+		Set_interrupt.eat_Lock = 0
+		Non_thr_sond("Sound/Vit_B.mp3")
 		print "I'm lack of vitamin B"
+		Set_interrupt.eat_Lock = 1
 	return "vitB"
