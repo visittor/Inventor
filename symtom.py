a5e337c48bbc5b8cf2a971161683ee53ad896149
visittor <chattarin.rodpon@gmail.com>
2017-05-02 16:10:08 +0700

    ...

--- symtom.py
+++ symtom.py
@@ -2,19 +2,19 @@ from graphic import Sound,Non_thr_sond
 from input import Set_interrupt
 def symtomA(inhibitor):
 	if inhibitor['vitA'] < 0.5:
-		Set_interrupt.play_list.append("Sound/Vit_K.mp3")
+		Set_interrupt.play_list.append("Sound/Vit_A.mp3")
 		print "I'm lack of vitamin A"
 	return "vitA"
 
 def symtomD(inhibitor):
 	if inhibitor['vitD'] < 0.5:
-		Set_interrupt.play_list.append("Sound/Vit_K.mp3")
+		Set_interrupt.play_list.append("Sound/Vit_D.mp3")
 		print "I'm lack of vitamin D"
 	return "vitD"
 
 def symtomE(inhibitor):
 	if inhibitor['vitE'] < 0.5:
-		Set_interrupt.play_list.append("Sound/Vit_K.mp3")
+		Set_interrupt.play_list.append("Sound/Vit_E.mp3")
 		print "I'm lack of vitamin E"
 	return "vitE"
 
@@ -26,12 +26,12 @@ def symtomK(inhibitor):
 
 def symtomC(inhibitor):
 	if inhibitor['vitC'] < 0.5:
-		Set_interrupt.play_list.append("Sound/Vit_K.mp3")
+		Set_interrupt.play_list.append("Sound/Vit_C.mp3")
 		print "I'm lack of vitamin C"
 	return "vitC"
 
 def symtomB(inhibitor):
 	if inhibitor['vitB'] < 0.5:
-		Set_interrupt.play_list.append("Sound/Vit_K.mp3")
+		Set_interrupt.play_list.append("Sound/Vit_B.mp3")
 		print "I'm lack of vitamin B"
 	return "vitB"
